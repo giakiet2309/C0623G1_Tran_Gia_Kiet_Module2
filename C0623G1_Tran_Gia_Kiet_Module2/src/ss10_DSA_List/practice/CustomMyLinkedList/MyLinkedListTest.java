85a6f17e4b90fd40525ea39e508f9ac90d56ebd7
giakiet2309 <giakhiemton@gmail.com>
2023-08-14 13:36:18 +0700

    nop bai tap

---
+++ C0623G1_Tran_Gia_Kiet_Module2/src/ss10_DSA_List/practice/CustomMyLinkedList/MyLinkedListTest.java
@@ -0,0 +1,19 @@
+package ss10_DSA_List.practice.CustomMyLinkedList;
+
+public class MyLinkedListTest {
+    public static void main(String[] args) {
+        MyLinkedList list1 = new MyLinkedList<String>("Phần tử đầu tiên");
+        list1.printList();
+        System.out.println("----------");
+        list1.add(3, "Phần tử add index 3");
+        list1.add(2, "Phần tử add index 2");
+        list1.add(1, "Phần tử add index 1");
+        list1.printList();
+        System.out.println("----------");
+        list1.addFirst("Phần tử add First");
+        list1.printList();
+        System.out.println("----------");
+
+
+    }
+}
