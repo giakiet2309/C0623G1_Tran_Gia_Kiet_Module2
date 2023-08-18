0c14b21006b360bdd18ba4740ee7bbd7b0542634
giakiet2309 <giakhiemton@gmail.com>
2023-08-18 11:38:23 +0700

    nop bai tap

--- C0623G1_Tran_Gia_Kiet_Module2/src/ss14_sortting_method/exercise/InsertSortStep.java
+++ C0623G1_Tran_Gia_Kiet_Module2/src/ss14_sortting_method/exercise/InsertSortStep.java
@@ -12,8 +12,9 @@ public class InsertSortStep {
     }
 
     public static int[] InsertSort(int[] sortList) {
+        int insertItem = 0;
         for (int i = 1; i < sortList.length; i++) {
-            int insertItem = sortList[i];
+            insertItem = sortList[i];
             for (int j = i - 1; j >= 0; j--) {
                 if (insertItem >= sortList[j]) {
                     sortList[j + 1] = insertItem;
--- C0623G1_Tran_Gia_Kiet_Module2/src/ss14_sortting_method/exercise/insert_sort/InsertSort.java
+++ C0623G1_Tran_Gia_Kiet_Module2/src/ss14_sortting_method/exercise/insert_sort/InsertSort.java
@@ -3,8 +3,9 @@ package ss14_sortting_method.exercise.insert_sort;
 
 public class InsertSort {
     public static int[] InsertSort(int[] sortList) {
+        int insertItem = 0;
         for (int i = 1; i < sortList.length; i++) {
-            int insertItem = sortList[i];
+            insertItem = sortList[i];
             for (int j = i - 1; j >= 0; j--) {
                 if (insertItem >= sortList[j]) {
                     sortList[j + 1] = insertItem;
